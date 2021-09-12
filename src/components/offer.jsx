fb2a75f56754484514cdab35babec8b599697fee
Marie Allpress <digitalinkfish@gmail.com>
2021-09-12 10:31:12 -0700

    created separate repository

---
+++ src/components/offer.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+export default class Offer extends Component {
+    render() {
+        return (
+            <div>
+
+
+                <section className="colorlib-offer" data-section="offer">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">What We Offer</span>
+                                <h2 className="colorlib-heading">Equipment</h2>
+                            </div>
+                        </div>
+
+                        <div className="row row-pt-md">
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-1">
+                                    <span className="icon">
+                                        <i className="icon-phone3" />
+                                    </span>
+
+                                    
+
+
+                                    <div className="desc">
+                                        <h3> Boxing Ring </h3>
+                                        <p></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-3">
+                                    <span className="icon">
+                                        <i className="icon-weights" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3> Fitness Equipment </h3>
+                                        <p></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-5">
+                                    <span className="icon">
+                                        <i className="" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3> Personal Training </h3>
+                                        <p></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+
