9682b7a8eacb001c3edd5b70385e076aefcec6a5
Marie Allpress <digitalinkfish@gmail.com>
2021-09-25 10:23:39 -0700

    changing offer section

--- src/components/offer.jsx
+++ src/components/offer.jsx
@@ -4,18 +4,50 @@ export default class Offer extends Component {
     render() {
         return (
             <div>
-
-
                 <section className="colorlib-offer" data-section="offer">
                     <div className="colorlib-narrow-content">
                         <div className="row">
                             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                                 <span className="heading-meta">What We Offer</span>
-                                <h2 className="colorlib-heading">Equipment</h2>
+                                <h2 className="colorlib-heading animate-box">Resources</h2>
                             </div>
                         </div>
 
-                        <div className="row row-pt-md">
+
+                        <div className="row">
+                            <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+                                <div class="project" style={{ backgroundImage: 'url(images/img-1.jpg)' }} >
+                                    <div className="desc">
+                                        <div className="con">
+                                            <h3>Boxing</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
+                                <div class="project" style={{ backgroundImage: 'url(images/img-2.jpg)' }} >
+                                    <div className="desc">
+                                        <div className="con">
+                                            <h3>Fitness Equipment</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-md-6 animate-box" data-animate-effect="fadeInBottom">
+                                    <div class="project" style={{ backgroundImage: 'url(images/img-4.jpg)' }} >
+                                        <div class="desc">
+                                            <div className="con">
+                                                <h3>Personal Training</h3>
+                                                <span className>Meet the trainers</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+
+                        {/*     <div className="row row-pt-md">
                             <div className="col-md-4 text-center animate-box">
                                 <div className="services color-2">
                                     <span className="icon">
@@ -53,7 +85,7 @@ export default class Offer extends Component {
                                 </div>
                             </div>
 
-                        </div>
+                        </div> */}
                     </div>
                 </section>
             </div>
